44c662c5e7300659f667d24e4a455e2d3db8f67e
Daisuke Inoue <hibariyashiki@gmail.com>
2018-04-29 22:04:26 +0900

    bin

---
+++ bin/now_5_5.rb
@@ -0,0 +1,12 @@
+require 'bundler/setup'
+require 'date'
+
+base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+lib_dir  = File.join(base_dir, "lib")
+
+require(File.join(lib_dir, 'Minatohasi.rb'))
+
+now = DateTime.now.new_offset(Rational(1, 24))
+
+rd = Minatohasi::rd(now.year, now.yday, now.hour, now.min, now.sec)
+print str = sprintf('%#.10g', rd)
